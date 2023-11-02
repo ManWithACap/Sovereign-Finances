c0ca8273ea80b44a83910e9b33675c34b1da7c32
ManWithACap <scottb03202004@gmail.com>
2023-11-02 10:03:11 -0400

    Able to read QR Code and use camera.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,9 @@
-﻿namespace SovereignFinances
+﻿using Image = System.Drawing.Image;
+using Point = System.Drawing.Point;
+using Size = System.Drawing.Size;
+using SizeF = System.Drawing.SizeF;
+
+namespace SovereignFinances
 {
     partial class SFT_Form
     {
@@ -28,19 +33,107 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SFT_Form));
+            B_takePhoto = new Button();
+            B_scan = new Button();
+            PB_photo = new PictureBox();
+            PB_captured = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)PB_photo).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)PB_captured).BeginInit();
             SuspendLayout();
+            // 
+            // B_takePhoto
+            // 
+            B_takePhoto.Location = new Point(302, 169);
+            B_takePhoto.Name = "B_takePhoto";
+            B_takePhoto.Size = new Size(75, 23);
+            B_takePhoto.TabIndex = 0;
+            B_takePhoto.Text = "Take Photo";
+            B_takePhoto.UseVisualStyleBackColor = true;
+            B_takePhoto.Click += B_takePhoto_Click;
+            // 
+            // B_scan
+            // 
+            B_scan.Location = new Point(383, 169);
+            B_scan.Name = "B_scan";
+            B_scan.Size = new Size(75, 23);
+            B_scan.TabIndex = 1;
+            B_scan.Text = "Scan";
+            B_scan.UseVisualStyleBackColor = true;
+            B_scan.Click += B_scan_Click;
+            // 
+            // PB_photo
+            // 
+            PB_photo.BackgroundImageLayout = ImageLayout.Center;
+            PB_photo.Image = (Image)resources.GetObject("PB_photo.Image");
+            PB_photo.InitialImage = null;
+            PB_photo.Location = new Point(12, 28);
+            PB_photo.Name = "PB_photo";
+            PB_photo.Size = new Size(240, 135);
+            PB_photo.SizeMode = PictureBoxSizeMode.StretchImage;
+            PB_photo.TabIndex = 2;
+            PB_photo.TabStop = false;
+            // 
+            // PB_captured
+            // 
+            PB_captured.BackgroundImageLayout = ImageLayout.Center;
+            PB_captured.Image = (Image)resources.GetObject("PB_captured.Image");
+            PB_captured.InitialImage = null;
+            PB_captured.Location = new Point(258, 28);
+            PB_captured.Name = "PB_captured";
+            PB_captured.Size = new Size(240, 135);
+            PB_captured.SizeMode = PictureBoxSizeMode.StretchImage;
+            PB_captured.TabIndex = 3;
+            PB_captured.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(116, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(29, 15);
+            label1.TabIndex = 4;
+            label1.Text = "LIVE";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(354, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Picture";
             // 
             // SFT_Form
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(PB_captured);
+            Controls.Add(PB_photo);
+            Controls.Add(B_scan);
+            Controls.Add(B_takePhoto);
             Name = "SFT_Form";
             Text = "Sovereign Finances Terminal";
+            FormClosing += MainForm_FormClosing;
             Load += SFT_Form_Load;
+            ((System.ComponentModel.ISupportInitialize)PB_photo).EndInit();
+            ((System.ComponentModel.ISupportInitialize)PB_captured).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button B_takePhoto;
+        private Button B_scan;
+        private PictureBox PB_photo;
+        private PictureBox PB_captured;
+        private Label label1;
+        private Label label2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,19 @@
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Diagnostics;
+using System.Windows.Forms;
+using IronBarCode;
+
 namespace SovereignFinances
 {
+
     public partial class SFT_Form : Form
     {
+        private FilterInfoCollection videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+        private VideoCaptureDevice videoSource;
+        private Bitmap capturedImage; // from videoSource's means
+        private string barcodeText;
+
         public SFT_Form()
         {
             InitializeComponent();
@@ -9,7 +21,37 @@ namespace SovereignFinances
 
         private void SFT_Form_Load(object sender, EventArgs e)
         {
+            videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            videoSource.NewFrame += new NewFrameEventHandler(videoSource_NewFrame);
+            videoSource.Start();
+        }
 
+        private void B_takePhoto_Click(object sender, EventArgs e)
+        {
+            PB_captured.Image = capturedImage;
+        }
+
+        private void videoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            capturedImage = (Bitmap)eventArgs.Frame.Clone();
+            // Display the image in the PictureBox
+            PB_photo.Image = (Bitmap)capturedImage.Clone();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+                videoSource = null;
+            }
+        }
+
+        private async void B_scan_Click(object sender, EventArgs e)
+        {
+            var barcode = await BarcodeReader.ReadAsync(PB_captured.Image);
+            barcodeText = barcode.First().Text;
         }
     }
 }
